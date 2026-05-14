23db6da0c255ceaa5ff33ec5b010ddfab7fa4146
Hafiza Naba <naba43152@gmail.com>
2026-05-14 11:43:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var age =("16 years old");
+alert( "I am " + age);
+var num = ("You have visited this site 14 times");
+alert(num);
+
+var birthYear =("2010");
+document.write("My Birth year is " + birthYear + "<br>"  +"Data typeof my declared variable in numbers" + "<br>") ;
+var visitorName = ("John doe");
+var productTitle = ("T-shirts(s)");
+var Quantity =("5");
+document.write( " <b>" +  visitorName + "</b>" + " " + "Ordered" +" " +  " <b>" + Quantity +  "</b>" +" " +  " " + "<b>"+ productTitle +   "</b>"  +" "  + "on XYZ Clothing store ");
